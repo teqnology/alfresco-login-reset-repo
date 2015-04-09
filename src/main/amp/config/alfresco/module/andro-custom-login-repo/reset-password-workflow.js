102a0f79ace8bc0f9afd2f4fc9636e0f10b3e4dd
Alen Subat <alen.subat@androgogic.com>
2015-04-09 14:19:51 +1000

    scheduled action, and bootstrap script to end reset-password workflow tasks with expired due date. Cleared log4j comments as well.

---
+++ src/main/amp/config/alfresco/module/andro-custom-login-repo/reset-password-workflow.js
@@ -0,0 +1,17 @@
+var today = new Date();
+function endExpiredWorkflow(){
+    var t, a, k, d;
+    var wf = workflow.getAssignedTasks();
+    for(var i = 0; i < wf.length; i++){
+        a = "activiti$" + wf[i].properties["bpm:taskId"];
+        k = wf[i].properties["bpm:description"];
+        d = wf[i].properties["bpm:dueDate"];
+        n = today.getTime();
+        d = d.getTime();
+        if(n>d){
+            var t = workflow.getTask(a);
+            t.endTask("Next");
+            logger.log("Activiti workflow ended. Task id: " + a + ". Activiti key: " + k);
+        }
+    }
+}
